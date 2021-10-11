57989cc91ec7766e42b7fcecbae1767e6b578446
Yussel Rosario <yusselrosario@gmail.com>
2021-10-11 18:47:04 -0400

    Initial commit master branch

---
+++ src/main/java/com/mymovieplan/api/MyMoviePlanBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.mymovieplan.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoviePlanBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoviePlanBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymovieplan/api/MyMoviePlanBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymovieplan.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoviePlanBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
