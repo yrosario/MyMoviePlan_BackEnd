3c4ba145b2791a66a727a13df47c8dd687a1be11
Yussel Rosario <yusselrosario@gmail.com>
2021-10-12 21:35:28 -0400

    Create Purchase entity class

---
+++ src/main/java/com/mymovieplan/api/model/Purchase.java
@@ -0,0 +1,68 @@
+package com.mymovieplan.api.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="purchase")
+public class Purchase {
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private final Long id;
+		
+		@ManyToOne
+		@Column(name="user_id")
+		private User user;
+		
+		@Column(name="order_date")
+		private Date orderDate;
+
+		public Purchase() {
+			this.id = null;
+		}
+
+		public Purchase(User user, Date orderDate) {
+			this();
+			this.user = user;
+			this.orderDate = orderDate;
+		}
+
+		public User getUser() {
+			return user;
+		}
+
+		public void setUser(User user) {
+			this.user = user;
+		}
+
+		public Date getOrderDate() {
+			return orderDate;
+		}
+
+		public void setOrderDate(Date orderDate) {
+			this.orderDate = orderDate;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		@Override
+		public String toString() {
+			return "Purchase [id=" + id + ", user=" + user + ", orderDate=" + orderDate + "]";
+		}
+		
+		
+		
+		
+		
+		
+}
