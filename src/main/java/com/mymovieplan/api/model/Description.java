1fd66fac0ce2bd9e0b5a4f6d04b3d5eb2d7f06b7
Yussel Rosario <yusselrosario@gmail.com>
2021-10-12 22:30:15 -0400

    Created description entity class

---
+++ src/main/java/com/mymovieplan/api/model/Description.java
@@ -0,0 +1,63 @@
+package com.mymovieplan.api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="description")
+public class Description {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private final Long id;
+	
+	@ManyToOne
+	@Column(name="movie_id")
+	private Movie movie;
+	
+	@Column(name="description")
+	private String description;
+
+	public Description() {
+		this.id = null;
+	}
+
+	public Description(Movie movie, String description) {
+		this();
+		this.movie = movie;
+		this.description = description;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Description [id=" + id + ", movie=" + movie + ", description=" + description + "]";
+	}
+	
+	
+	
+}
