ff5cbe4888604ae05c9fcf3510cb6c97e2ee1ddd
Yussel Rosario <yusselrosario@gmail.com>
2021-10-12 20:38:23 -0400

    Create User entity class

---
+++ src/main/java/com/mymovieplan/api/model/User.java
@@ -0,0 +1,147 @@
+package com.mymovieplan.api.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private final Long id;
+	
+	@Column(name="first_name")
+	private String fName;
+	
+	@Column(name="last_name")
+	private String lName;
+	
+	@Column(name="address")
+	private String address;
+	
+	@Column(name="city")
+	private String city;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="birthday")
+	private Date birthday;
+	
+	@Column(name="pasword")
+	private String password;
+	
+	@Column(name="role")
+	private String role;
+	
+	@Column(name="active")
+	private Boolean active;
+
+	public User() {
+		this.id = null;
+		active = true;
+	}
+
+	public User(String fName, String lName, String address, String city, String email, 
+			    Date birthday, String password, String role, Boolean active) {
+		this();
+		this.fName = fName;
+		this.lName = lName;
+		this.address = address;
+		this.city = city;
+		this.email = email;
+		this.birthday = birthday;
+		this.password = password;
+		this.role = role;
+		this.active = active;
+	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	
+	
+	
+	
+	
+}
