fb0fad7eac2b617dada2a7b41fd4d27c089d77e3
Yussel Rosario <yusselrosario@gmail.com>
2021-10-12 21:57:02 -0400

    Create Cart entity class

---
+++ src/main/java/com/mymovieplan/api/model/Cart.java
@@ -0,0 +1,63 @@
+package com.mymovieplan.api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cart")
+public class Cart {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private final Long id;
+	
+	@OneToOne
+	@Column(name="user_id")
+	private User user;
+	
+	@Column(name="nums_of_items")
+	private Integer numberOfItems;
+
+	public Cart() {
+		this.id = null;
+	}
+
+	public Cart(User user) {
+		this();
+		this.user = user;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+
+	public Integer getNumberOfItems() {
+		return numberOfItems;
+	}
+
+	public void setNumberOfItems(Integer numberOfItems) {
+		this.numberOfItems = numberOfItems;
+	}
+
+	@Override
+	public String toString() {
+		return "Cart [id=" + id + ", user=" + user + ", numberOfItems=" + numberOfItems + "]";
+	}
+	
+	
+	
+}
