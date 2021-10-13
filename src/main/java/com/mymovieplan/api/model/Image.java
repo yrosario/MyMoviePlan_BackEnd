35de04f864f07c94f4f273cba5ae42c38aff9716
Yussel Rosario <yusselrosario@gmail.com>
2021-10-12 22:38:45 -0400

    Created Image entity class

---
+++ src/main/java/com/mymovieplan/api/model/Image.java
@@ -0,0 +1,68 @@
+package com.mymovieplan.api.model;
+
+import java.util.Arrays;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="image")
+public class Image {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private final Long id;
+	
+	@ManyToOne
+	@Column(name="movie_id")
+	private Movie movie;
+	
+	@Column(name="image")
+	private byte[] image;
+
+	public Image() {
+		this.id = null;
+	}
+
+	public Image(Movie movie, byte[] image) {
+		this();
+		this.movie = movie;
+		this.image = image;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Image [id=" + id + ", movie=" + movie + ", image=" + Arrays.toString(image) + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
