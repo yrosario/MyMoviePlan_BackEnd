4ee72b574d9f45c874c7077eb237710d5554bf1e
Yussel Rosario <yusselrosario@gmail.com>
2021-10-14 20:59:11 -0400

    Created PurchaseItem entity and added cardinality to Purchase and Movie entity

---
+++ src/main/java/com/mymovieplan/api/model/PurchaseItem.java
@@ -0,0 +1,65 @@
+package com.mymovieplan.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="purchase_item")
+public class PurchaseItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private final Long id;
+	
+	@ManyToOne
+	private Purchase purchase;
+	
+	@ManyToOne
+	private Movie movie;
+
+	public PurchaseItem() {
+		this.id = null;
+	}
+
+	public PurchaseItem(Purchase purchase, Movie movie) {
+		this();
+		this.purchase = purchase;
+		this.movie = movie;
+	}
+
+	public Purchase getPurchase() {
+		return purchase;
+	}
+
+	public void setPurchase(Purchase purchase) {
+		this.purchase = purchase;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	@Override
+	public String toString() {
+		return "PurchaseItem [id=" + id + ", purchase=" + purchase + ", movie=" + movie + "]";
+	}
+	
+	
+	
+	
+
+}
