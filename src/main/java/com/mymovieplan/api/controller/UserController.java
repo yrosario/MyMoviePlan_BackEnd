0afc20f712012fc7ba29c9784d458227f07fa899
Yussel Rosario <yusselrosario@gmail.com>
2021-10-29 01:01:53 -0400

    Created ExceptionResponse.java and CustomizeResponseException.java

--- src/main/java/com/mymovieplan/api/controller/UserController.java
+++ src/main/java/com/mymovieplan/api/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mymovieplan.api.exception.UserNotFoundException;
 import com.mymovieplan.api.model.User;
 import com.mymovieplan.api.service.*;
 
@@ -16,6 +17,10 @@ public class UserController {
 	
 	@GetMapping(path = "/user/{id}")
 	public User getUser(@PathVariable("id") Long id){
+		
+		User user = userService.getUser(id);
+		if(user == null)
+			throw new UserNotFoundException("id: -" +id);
 		return userService.getUser(id);
 	}
 }
---
+++ src/main/java/com/mymovieplan/api/exception/CustomizeResponseEnityExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.mymovieplan.api.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomizeResponseEnityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+		
+		ExceptionResponse exceptionResponse = 
+				new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) {
+		
+		ExceptionResponse exceptionResponse = 
+				new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+}
